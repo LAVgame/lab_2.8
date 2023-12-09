b1e88a0e18b51024523515083c4a871936ddf9cb
L_A_V_game <vipaleksei007@gmail.com>
2023-12-09 19:08:53 +0300

    erw

---
+++ works/init.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# workrs/__init__.py
+
+__all__ = ['workers']
---
+++ works/main_program.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# main_program.py
+
+from workers_package import workers
+
+if __name__ == '__main__':
+    workers.main()
